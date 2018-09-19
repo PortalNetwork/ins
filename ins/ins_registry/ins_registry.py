aec28e2c71f97dad45f19213000e827185b7e17d
Phyrex Tsai <x01ep23i@hotmail.com>
2018-09-19 12:49:29 +0800

    Add test flow and transaction data for INS contracts

--- ins/ins_registry/ins_registry.py
+++ ins/ins_registry/ins_registry.py
@@ -1,82 +1,74 @@
 from iconservice import *
 from Crypto.Hash import keccak
-
+import re
 
 class INSRegistry(IconScoreBase):
 
-    __OWNER = 'owner'
+    __HOLDER = 'holder'
     __RESOLVER = 'resolver'
     __TTL = 'ttl'
 
     @eventlog(indexed=2)
-    def Transfer(self, node: bytes, owner: Address): pass
+    def Transfer(self, node: str, holder: Address): pass
 
     @eventlog(indexed=3)
-    def NewOwner(self, node: bytes, label: bytes, owner: Address): pass
+    def NewHolder(self, node: str, label: str, holder: Address): pass
 
     @eventlog(indexed=2)
-    def NewResolver(self, node: bytes, resolver: Address): pass
+    def NewResolver(self, node: str, resolver: Address): pass
 
     @eventlog(indexed=2)
-    def NewTTL(self, node: bytes, ttl: int) : pass
+    def NewTTL(self, node: str, ttl: int) : pass
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
         # Use DictDB to store value as map
-        self.__owner = DictDB(self.__OWNER, db, value_type=Address)
+        self.__holder = DictDB(self.__HOLDER, db, value_type=Address)
         self.__resolver = DictDB(self.__RESOLVER, db, value_type=Address)
         self.__ttl = DictDB(self.__TTL, db, value_type=int)
 
     def on_install(self) -> None:
         super().on_install()
-        # initial all the node owner is the contract owner
-        # after deploy need to change 0x0 to INSRegistrar contract address
-        self.__owner['0x0'] = self.msg.sender
+        # initial all the node holder is the contract holder
+        # top-level domain: icon
+        self.__holder["icon"] = self.msg.sender
 
     def on_update(self) -> None:
         super().on_update()
 
     @external(readonly=True)
-    def owner(self, node: bytes) -> Address:
-        return self.__owner[node]
+    def holder(self, node: str) -> Address:
+        return self.__holder[node]
 
     @external(readonly=True)
-    def resolver(self, node: bytes) -> Address:
+    def resolver(self, node: str) -> Address:
         return self.__resolver[node]
 
     @external(readonly=True)
-    def ttl(self, node: bytes) -> int:
+    def ttl(self, node: str) -> int:
         return self.__ttl[node]
 
     @external
-    def setOwner(self, node: bytes, addr: Address) -> None:
-        if self.__owner[node] == self.msg.sender:
+    def setHolder(self, node: str, addr: Address) -> None:
+        if self.__holder[node] == self.msg.sender:
             self.Transfer(node, addr)
-            self.__owner[node] = addr
+            self.__holder[node] = addr
 
     @external
-    def setSubnodeOwner(self, node: bytes, label: bytes, addr: Address) -> None:
-        if self.__owner[node] == self.msg.sender:
-            # TODO check keccak hash
-            subnode = self.namehash(node, label)
-            self.NewOwner(node, label, addr)
-            self.__owner[subnode] = addr
+    def setSubnodeHolder(self, node: str, label: str, addr: Address) -> None:
+        if self.__holder[node] == self.msg.sender and re.match("[a-z0-9]+", label):
+            subnode = label + "." + node
+            self.NewHolder(node, label, addr)
+            self.__holder[subnode] = addr
 
     @external
-    def setResolver(self, node: bytes, resolver: Address) -> None:
-        if self.__owner[node] == self.msg.sender:
+    def setResolver(self, node: str, resolver: Address) -> None:
+        if self.__holder[node] == self.msg.sender:
             self.NewResolver(node, resolver)
             self.__resolver[node] = resolver
 
     @external
-    def setTTL(self, node: bytes, ttl: int) -> None:
-        if self.__owner[node] == self.msg.sender:
+    def setTTL(self, node: str, ttl: int) -> None:
+        if self.__holder[node] == self.msg.sender:
             self.NewTTL(node, ttl)
             self.__ttl[node] = ttl
-
-    # Concate node and label
-    def namehash(self, node: bytes, label: bytes) -> bytes:
-        keccak256 = keccak.new(digest_bits=256)
-        keccak256.update(node + label)
-        hexdigest = keccak256.hexdigest()
-        return hexdigest
--- ins/ins_resolver/ins_resolver.py
+++ ins/ins_resolver/ins_resolver.py
@@ -4,7 +4,7 @@ from iconservice import *
 class INSRegistryInterface(InterfaceScore):
 
     @interface
-    def owner(self, node: bytes) -> Address: pass
+    def holder(self, node: str) -> Address: pass
 
 
 class INSResolver(IconScoreBase):
@@ -16,23 +16,23 @@ class INSResolver(IconScoreBase):
     __NAME = 'name'
 
     @eventlog(indexed=2)
-    def AddressChanged(self, node: bytes, addr: Address): pass
+    def AddressChanged(self, node: str, addr: Address): pass
 
     @eventlog(indexed=3)
-    def MultihashChanged(self, node: bytes, name: str, multihash: bytes): pass
+    def MultihashChanged(self, node: str, name: str, multihash: str): pass
 
     @eventlog(indexed=3)
-    def TextChanged(self, node: bytes, key: str, value: str): pass
+    def TextChanged(self, node: str, key: str, value: str): pass
 
     @eventlog(indexed=2)
-    def NameChanged(self, node: bytes, name: str): pass
+    def NameChanged(self, node: str, name: str): pass
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
 
         self.__addr_ins_registry_score = VarDB(self.__ADDR_INS_REGISTRY_SCORE, db, value_type=Address)
         self.__address = DictDB(self.__ADDRESS, db, value_type=Address)
-        self.__multihash = DictDB(self.__MULTIHASH, db, value_type=bytes, depth=2)
+        self.__multihash = DictDB(self.__MULTIHASH, db, value_type=str, depth=2)
         self.__text = DictDB(self.__TEXT, db, value_type=str, depth=2)
         self.__name = DictDB(self.__NAME, db, value_type=str)
 
@@ -40,7 +40,7 @@ class INSResolver(IconScoreBase):
             self.__addr_ins_registry_score.get(), INSRegistryInterface)
 
     # TODO ins_address should be defined
-    def on_install(self, ins_address: str = 'cx1390e811b2e0a9f2e5559ea26d8a39bc4b747a7a') -> None:
+    def on_install(self, ins_address: str = 'cx391a9a74b552198d90d4c0e11590373b030c6184') -> None:
         super().on_install()
 
         ins_registry_address = Address.from_string(ins_address)
@@ -52,42 +52,42 @@ class INSResolver(IconScoreBase):
         super().on_update()
 
     @external
-    def setAddr(self, node: bytes, addr: Address) -> None:
-        if self.__ins_registry_score.owner(node) == self.msg.sender:
+    def setAddr(self, node: str, addr: Address) -> None:
+        if self.__ins_registry_score.holder(node) == self.msg.sender:
             self.AddressChanged(node, addr)
             self.__address[node] = addr
 
     @external
-    def setMultihash(self, node: bytes, name: str, multihash: bytes) -> None:
-        if self.__ins_registry_score.owner(node) == self.msg.sender:
+    def setMultihash(self, node: str, name: str, multihash: str) -> None:
+        if self.__ins_registry_score.holder(node) == self.msg.sender:
             self.MultihashChanged(node, multihash)
             self.__multihash[node][name] = multihash
 
     @external
-    def setText(self, node: bytes, key: str, value: str) -> None:
-        if self.__ins_registry_score.owner(node) == self.msg.sender:
+    def setText(self, node: str, key: str, value: str) -> None:
+        if self.__ins_registry_score.holder(node) == self.msg.sender:
             self.TextChanged(node, key, value)
             self.__text[node][key] = value
 
     @external
-    def setName(self, node: bytes, name: str) -> None:
-        if self.__ins_registry_score.owner(node) == self.msg.sender:
+    def setName(self, node: str, name: str) -> None:
+        if self.__ins_registry_score.holder(node) == self.msg.sender:
             self.NameChanged(node, name)
             self.__name[node] = name
 
     @external(readonly=True)
-    def addr(self, node: bytes) -> Address:
+    def addr(self, node: str) -> Address:
         return self.__address[node]
 
     @external(readonly=True)
-    def multihash(self, node: bytes, name: str) -> bytes:
+    def multihash(self, node: str, name: str) -> str:
         return self.__multihash[node][name]
 
     @external(readonly=True)
-    def text(self, node: bytes, key: str) -> str:
+    def text(self, node: str, key: str) -> str:
         return self.__text[node][key]
 
     @external(readonly=True)
-    def name(self, node: bytes) -> str:
+    def name(self, node: str) -> str:
         return self.__name[node]
     
